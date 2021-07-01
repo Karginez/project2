ca2d2cee6fe8d7dd7fdf1065b015a1821c811c92
yehor <example@gmail.com>
2021-07-02 00:10:07 +0300

    home task 3 added

--- info/script.js
+++ info/script.js
@@ -82,95 +82,127 @@
 
 // Условия///////////////////////////////////////////////////////
 
-const num = 50;
+// const num = 50;
 
-if (num < 49) {
-    console.log('ERROR');
-} else if (num > 100) {
-    console.log('TOO MUCH');
-} else {
-    console.log('OK');
-}
+// if (num < 49) {
+//     console.log('ERROR');
+// } else if (num > 100) {
+//     console.log('TOO MUCH');
+// } else {
+//     console.log('OK');
+// }
 
-(num === 50) ? console.log('OK') : console.log('ERROR'); //тернарный оператор
+// (num === 50) ? console.log('OK') : console.log('ERROR'); //тернарный оператор
 
-const num2 = 52;
+// const num2 = 52;
 
-switch (num2) {   //строгое сравнение ===
-    case 49:
-        console.log('FAIL');
-        break;
-    case 50:
-        console.log('FAIL');
-        break;
-    case 51:
-        console.log('PASS');
-        break;
-    default:
-        console.log('wrong');
-        break;
-}
+// switch (num2) {   //строгое сравнение ===
+//     case 49:
+//         console.log('FAIL');
+//         break;
+//     case 50:
+//         console.log('FAIL');
+//         break;
+//     case 51:
+//         console.log('PASS');
+//         break;
+//     default:
+//         console.log('wrong');
+//         break;
+// }
 
-// ЦИКЛЫ ////////////////////////////////////////////////////////////
+// // ЦИКЛЫ ////////////////////////////////////////////////////////////
 
-let num3 = 50;
+// let num3 = 50;
 
-while (num3 < 55) {
-    console.log(num3); //WHILE
-    num3++;
-}
+// while (num3 < 55) {
+//     console.log(num3); //WHILE
+//     num3++;
+// }
 
-do {
-    console.log(num3); //DO - WHILE
-    num3++;
-}
-while (num3 < 55);
+// do {
+//     console.log(num3); //DO - WHILE
+//     num3++;
+// }
+// while (num3 < 55);
 
-let num4 = 50;
+// let num4 = 50;
 
-for (let i = 1; i < 8; i++) { //FOR
-    if (i === 6) {
-        //break;
-        continue;
-    }
-    console.log(i);
+// for (let i = 1; i < 8; i++) { //FOR
+//     if (i === 6) {
+//         //break;
+//         continue;
+//     }
+//     console.log(i);
  
 
-}
+// }
 
-//функции /////////////////////////////////////////////////////////
+// //функции /////////////////////////////////////////////////////////
 
-function showFirstMessage() {
-    console.log('hello');
-}
-showFirstMessage();
+// function showFirstMessage() {
+//     console.log('hello');
+// }
+// showFirstMessage();
 
-let num5 = 20; //глобальная переменная
-function showFirstMessage(text) {
-    console.log(text);
-    let num5 = 10; //локальная переменная
-    console.log(num5);
-}
-showFirstMessage('hello');
-console.log(num5);
+// let num5 = 20; //глобальная переменная
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num5 = 10; //локальная переменная
+//     console.log(num5);
+// }
+// showFirstMessage('hello');
+// console.log(num5);
 
-function calc(a, b) { //существует до отработки кода
-    return (a + b); //после ретурн код не отработает
-}
+// function calc(a, b) { //существует до отработки кода
+//     return (a + b); //после ретурн код не отработает
+// }
 
-console.log(calc(17, 5));
-console.log(calc(4, 9));
-console.log(calc(412, 57));
-
-
-const logger = function() { //сущетсвует только когда до нее дойдет очередь
-    console.log('hello');
-};
-logger();
-
-const calc2 = (a, b) => { return a + b }; //стрелочная функция
-const calc3 = (a, b) => a + b;
-const calc4 = a => a + b;
+// console.log(calc(17, 5));
+// console.log(calc(4, 9));
+// console.log(calc(412, 57));
 
 
+// const logger = function() { //сущетсвует только когда до нее дойдет очередь
+//     console.log('hello');
+// };
+// logger();
 
+// const calc2 = (a, b) => { return a + b }; //стрелочная функция
+// const calc3 = (a, b) => a + b;
+// const calc4 = a => a + b;
+
+
+//методы и свойства строк и чисел////////////////////////////////////////
+
+const str = 'test';
+const arr = [1, 2, 3];
+
+// console.log(str.length);
+// console.log(arr.length);
+// console.log(str[2]);
+console.log(str.toUpperCase());
+
+// const fruit = 'some frit';
+// // console.log(fruit.indexOf("fruit")); //поиск в строке, -1 не найдено
+
+// if (fruit.indexOf("fruit") == -1) {
+//     console.log('не найдено');
+// } else {
+//     console.log(fruit.indexOf("fruit"));
+// }
+
+const logg = "test word for cutting";
+
+console.log(logg.slice(5, 9)); // вырезает от и до
+
+console.log(logg.substring(5, 9));// вырезает от и до
+
+console.log(logg.substr(5, 4)); // от и сколько символов вырезать
+
+const num = 12.4;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
--- movie/script.js
+++ movie/script.js
@@ -1,36 +1,70 @@
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+    }
+}
+
+//start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: true
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?',''),
-          b = +prompt('На сколько оцените его?','');
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('data is OK');
-    } else {
-        console.log('error');
-        i--;
+function rememberMyfilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?',''),
+              b = +prompt('На сколько оцените его?','');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('data is OK');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
 
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert('Классический зритель');
-} else if (personalMovieDB.count >= 30) {
-    alert('Киноман');
-} else {
-    alert('Произошла ошибка');
+//rememberMyfilms();
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert('Просмотрено мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert('Классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        alert('Киноман');
+    } else {
+        alert('Произошла ошибка');
+    }
 }
-console.log(personalMovieDB);
+
+//detectPersonalLevel();
 
 
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        const a = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = a;
+    }
+}
 
+writeYourGenres();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
