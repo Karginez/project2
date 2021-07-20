355bf685a8f8e3a5d3fecf2e600bc1c121293fa2
yehor <example@gmail.com>
2021-07-20 21:19:03 +0300

    functions are added

--- info/script.js
+++ info/script.js
@@ -175,34 +175,212 @@
 
 //методы и свойства строк и чисел////////////////////////////////////////
 
-const str = 'test';
-const arr = [1, 2, 3];
+// const str = 'test';
+// const arr = [1, 2, 3];
 
-// console.log(str.length);
-// console.log(arr.length);
-// console.log(str[2]);
-console.log(str.toUpperCase());
+// // console.log(str.length);
+// // console.log(arr.length);
+// // console.log(str[2]);
+// console.log(str.toUpperCase());
 
-// const fruit = 'some frit';
-// // console.log(fruit.indexOf("fruit")); //поиск в строке, -1 не найдено
+// // const fruit = 'some frit';
+// // // console.log(fruit.indexOf("fruit")); //поиск в строке, -1 не найдено
 
-// if (fruit.indexOf("fruit") == -1) {
-//     console.log('не найдено');
-// } else {
-//     console.log(fruit.indexOf("fruit"));
+// // if (fruit.indexOf("fruit") == -1) {
+// //     console.log('не найдено');
+// // } else {
+// //     console.log(fruit.indexOf("fruit"));
+// // }
+
+// const logg = "test word for cutting";
+
+// console.log(logg.slice(5, 9)); // вырезает от и до
+
+// console.log(logg.substring(5, 9));// вырезает от и до
+
+// console.log(logg.substr(5, 4)); // от и сколько символов вырезать
+
+// const num = 12.4;
+// console.log(Math.round(num));
+
+// const test = '12.2px';
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
+
+
+// callback function ///////////////////////////////////////////////////
+
+function learnJS(lang, callback) {
+    console.log(`я учу: ${lang}`);
+    callback();
+}
+
+learnJS('JavaScript', function() {
+    console.log('я прошел урок');
+});
+
+const makePizza = function (title, cb) {
+    console.log(`Заказ на приготовление пиццы «${title}» получен. Начинаем готовить…`); 
+    setTimeout(cb, 3000);
+   }
+   
+   const readBook = function () {
+    console.log('Читаю книгу «Колдун и кристалл»…');
+   }
+   
+   const eatPizza = function () {
+     console.log('Ура! Пицца готова, пора подкрепиться.');
+   }
+   
+   makePizza('Пеперонни', eatPizza);
+   readBook();
+
+
+
+// обьекты, деструктуризация////////////////////////////////////////////
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     makeTest: function() {
+//         console.log('test'); //создание метода обьекта
+//     }
+// };
+
+// options.makeTest();
+
+//console.log(options.colors.border);
+
+//delete options.name;
+
+// метод перебора for/in
+// for (let key in options) {
+//     console.log(`свойство ${key} имеет значение ${options[key]}`);
 // }
 
-const logg = "test word for cutting";
+// // свойство name имеет значение test
+// // свойство width имеет значение 1024
+// // свойство height имеет значение 1024
+// // свойство colors имеет значение [object Object]
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }   
+// }
+// console.log(counter);
 
-console.log(logg.slice(5, 9)); // вырезает от и до
+// свойство name имеет значение test
+// свойство width имеет значение 1024
+// свойство height имеет значение 1024
+// свойство border имеет значение black
+// свойство bg имеет значение red
 
-console.log(logg.substring(5, 9));// вырезает от и до
+// console.log(Object.keys(options).length); //отображает все ключи в обьекте, подсчитывает их количество
 
-console.log(logg.substr(5, 4)); // от и сколько символов вырезать
 
-const num = 12.4;
-console.log(Math.round(num));
 
-const test = '12.2px';
-console.log(parseInt(test));
-console.log(parseFloat(test));
+//деструктуризация обьекта///////////////////
+
+
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('test'); //создание метода обьекта
+    }
+};
+
+const {border, bg} = options.colors;
+console.log(border);
+
+//массивы и псевдомассивы///////////////////////////
+
+const arr = [1, 2, 3, 6, 8];
+
+// arr.pop(); //удаляет последний элемент массива
+
+// arr.push(10); //добавить чтото в конец
+
+// for (let i = 0; i < arr.length; i++) { //перебор массива обычным циклом
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) { //перебор методом for..of //можно использовать brake, continueв отличии от forEach
+//     console.log(value);
+// }
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// const str = 'eeee, errttt, yyyyyy, uuuuuuu, iiiiii';
+// const products = str.split(', '); //преображение в массив, разделитель указан в скобках
+// products.sort();
+// console.log(products);
+// //console.log(products.join('; ')); //массив в строку
+
+// const arr2 = [1, 20, 31, 6, 8];
+// arr2.sort(compareNum);
+
+// function compareNum(a, b) { //сорт сортирует как стринг, для чисел нужно применять такую функцию
+//     return a - b;
+// }
+
+// console.log(arr2);
+
+// // передача по ссылке и значению ///////////////////////////////
+
+// let a = 5,
+//     b = a;
+
+// b = b + 5;
+
+const obj = {
+    a: 5,
+    b: 1
+};
+
+// const copy = obj; //ссылка на существующий обьект, модифицируя копию модифицируется первоначальный обьект
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
